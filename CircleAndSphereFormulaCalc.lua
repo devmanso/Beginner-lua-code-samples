0286f49d75fdbd238e38eefbd65ea93f325d35ae
Planebagels <64249106+Planebagels@users.noreply.github.com>
2020-10-20 16:31:04 -0300

    added files

---
+++ CircleAndSphereFormulaCalc.lua
@@ -0,0 +1,19 @@
+print("Circle and sphere Formula calculator")
+print(" ")--Im too bohered to write newline
+print("what is the radius of the circle")
+rad = io.read("*n")
+
+c = 2 * math.pi * rad^2
+print("Area of Curcumfrence ="..c)
+
+a = math.pi * rad^2
+print("Circle Area = "..a)
+
+s = 4 * math.pi * rad^2
+print("Sphere Surface area ="..s)
+
+v = 4 / 3 * math.pi * rad^3
+print("Sphere volume = "..v)
+
+-- I wrote this while I was in school because I was tired of doing it manually.
+-- this is from my old repo https://github.com/Planebagels/Circle-and-sphere-Formula-calculator
