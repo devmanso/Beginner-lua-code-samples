8f8676269df0a97c318aefc2c5e6b382a735a0a6
Planebagels <64249106+Planebagels@users.noreply.github.com>
2020-10-20 13:58:28 -0300

    added files

---
+++ tables_in_.lua
@@ -0,0 +1,16 @@
+-- tables in lua
+
+firstname = "firstname"
+
+table = {firstname}
+
+
+--tables are indexed at one!!!11112
+table.y = "poo ".."pee"
+table.x = 123 -- all int vars are actually doubles
+table.z = 546.78
+
+print(table.y)
+
+
+-- lua like python is dynamicly typed
