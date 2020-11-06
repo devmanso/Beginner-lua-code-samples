93e314fe2a6b450b2639b0bc134e1e25353d9be6
Planebagels <64249106+Planebagels@users.noreply.github.com>
2020-11-06 12:57:07 -0500

    updated files

--- helloworld.lua
+++ helloworld.lua
@@ -11,3 +11,15 @@ print "hello world" -- this works fine because lua doesn't care about whitespace
 print "hello world";
 
 io.write("hello world")
+
+print("hello", "world");
+
+io.write("hello", "world"); -- this will output "helloworld" so add a space like this
+
+io.write("hello", " " "world"); -- this works without the semi-colon it is not neccesary in lua
+
+print("hello", "world")
+
+io.write("hello", "world")
+
+io.write("hello", " " "world")
